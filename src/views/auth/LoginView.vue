9ac0723e2f24cef50603afe67cb96f67ff6bc2ff
johnleighbeltran <johnleighbeltran@gmail.com>
2025-04-08 16:14:39 +0800

    adding applayout

--- src/views/auth/LoginView.vue
+++ src/views/auth/LoginView.vue
@@ -1,63 +1,36 @@
 <script setup>
-import { ref } from 'vue'
-
-const theme = ref('light')
-
-function onClick() {
-  theme.value = theme.value === 'light' ? 'dark' : 'light'
-}
+import appLayout from '@/components/layout/AppLayout.vue'
 </script>
 
 <template>
-  <v-responsive class="border rounded">
-    <v-app :theme="theme">
-      <v-app-bar class="px-3" color="grey-lighten-1">
-        <v-spacer></v-spacer>
+  <appLayout>
+    <template #content>
+      <v-row>
+        <v-col cols="12" md="6" class="mx-auto pt-16">
+          <v-card class="mx-auto" color="orange-lighten-4" elevation="2">
+            <template v-slot:title>
+              <v-img src="/images/logofav.png"></v-img>
+            </template>
 
-        <v-btn
-          :prepend-icon="theme === 'light' ? 'mdi-weather-sunny' : 'mdi-weather-night'"
-          text="Toggle Theme"
-          slim
-          @click="onClick"
-        ></v-btn>
-      </v-app-bar>
+            <v-card-text class="bg-orange-lighten-5 pt-4">
+              <v-form fast-fail @submit.prevent>
+                <v-text-field label="Email"></v-text-field>
 
-      <v-main class="bg-amber-lighten-5">
-        <v-container>
-          <v-row>
-            <v-col cols="12" md="6" class="mx-auto pt-16">
-              <v-card
-                class="mx-auto"
-                color="orange-lighten-4"
-                elevation="2"
-              >
-                <template v-slot:title>
-                  <v-img src="/images/logofav.png"></v-img>
-                </template>
+                <v-text-field label="Password" type="password"></v-text-field>
 
-                <v-card-text class="bg-orange-lighten-5 pt-4">
-                  <v-form fast-fail @submit.prevent>
-                    <v-text-field label="Email"></v-text-field>
+                <v-btn class="mt-2" variant="tonal" type="submit" block>Submit</v-btn>
+              </v-form>
 
-                    <v-text-field label="Password" type="password"></v-text-field>
+              <v-divider class="my-5"></v-divider>
 
-                    <v-btn class="mt-2" variant="tonal" type="submit" block>Submit</v-btn>
-                  </v-form>
-
-                  <v-divider class="my-5"></v-divider>
-
-                  <h5>
-                    Don't have account?
-                    <router-link to="/register">Click Here to Register</router-link>
-                  </h5>
-                </v-card-text>
-              </v-card>
-            </v-col>
-          </v-row>
-        </v-container>
-      </v-main>
-
-      <v-footer color="amber-lighten-5" border app>2025 - LostnFound</v-footer>
-    </v-app>
-  </v-responsive>
+              <h5>
+                Don't have account?
+                <router-link to="/register">Click Here to Register</router-link>
+              </h5>
+            </v-card-text>
+          </v-card>
+        </v-col>
+      </v-row>
+    </template>
+  </appLayout>
 </template>
