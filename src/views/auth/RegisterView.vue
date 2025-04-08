7a9b1da5f62cbac2ad85e64e432890065c6c6e82
johnleighbeltran <johnleighbeltran@gmail.com>
2025-04-08 14:54:30 +0800

    color palette changes; card-elavated

--- src/views/auth/RegisterView.vue
+++ src/views/auth/RegisterView.vue
@@ -28,8 +28,8 @@ function onClick() {
             <v-col cols="12" md="6" class="mx-auto pt-16">
               <v-card
                 class="mx-auto"
-                prepend-icon="mdi-account"
-                subtitle="Login"
+                prepend-icon="mdi-exit-to-app"
+                subtitle="Register"
                 color="orange-lighten-4"
                 elevation="2"
               >
@@ -39,18 +39,20 @@ function onClick() {
 
                 <v-card-text class="bg-orange-lighten-5 pt-4">
                   <v-form fast-fail @submit.prevent>
+                    <v-text-field label="Firstname"></v-text-field>
+                    <v-text-field label="Lastname"></v-text-field>
+
                     <v-text-field label="Email"></v-text-field>
 
                     <v-text-field label="Password" type="password"></v-text-field>
+                    <v-text-field label="Password Confirmation" type="password"></v-text-field>
 
                     <v-btn class="mt-2" variant="tonal" type="submit" block>Submit</v-btn>
                   </v-form>
-
                   <v-divider class="my-5"></v-divider>
 
-                  <h5>
-                    Don't have account?
-                    <router-link to="/register">Click Here to Register</router-link>
+                  <h5 class="text-center">
+                    Already have account? <RouterLink to="/login">Click Here to Login</RouterLink>
                   </h5>
                 </v-card-text>
               </v-card>
