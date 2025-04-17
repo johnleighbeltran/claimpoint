c0193505042e60ddd1c997ca5da022f180996c14
johnleighbeltran <johnleighbeltran@gmail.com>
2025-04-17 13:43:19 +0800

    remove light&dark switch and clean up code

--- src/views/auth/RegisterView.vue
+++ src/views/auth/RegisterView.vue
@@ -1,25 +1,16 @@
 <script setup>
 import { ref } from 'vue'
 
-const theme = ref('light')
 
-function onClick() {
-  theme.value = theme.value === 'light' ? 'dark' : 'light'
-}
 </script>
 
 <template>
   <v-responsive class="border rounded">
-    <v-app :theme="theme">
+    <v-app>
       <v-app-bar class="px-3" color="grey-lighten-1">
         <v-spacer></v-spacer>
 
-        <v-btn
-          :prepend-icon="theme === 'light' ? 'mdi-weather-sunny' : 'mdi-weather-night'"
-          text="Toggle Theme"
-          slim
-          @click="onClick"
-        ></v-btn>
+
       </v-app-bar>
 
       <v-main class="bg-amber-lighten-5">
